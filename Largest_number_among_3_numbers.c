88012b5fa378ae8516e62bfbd61fcfc53b784116
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2022-12-19 21:27:51 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>b) { if(a>c)     {         printf("%d",a);         }         else         {             printf("%d",c);
            }
            }
            else { if(b>c)
            {
                printf("%d",b);
                }
                else
                {
                    printf("%d",c); }
                    }
    }

--- Largest_number_among_3_numbers.c
+++ Largest_number_among_3_numbers.c
@@ -2,29 +2,23 @@
 int main()
 {
     int a,b,c;
-    scanf("%d%d%d",&a,&b,&c);
-    if(a>b)
+    scanf("%d%d%d",&a,&b,&c); 
+    if(a>b) { if(a>c)
     {
-        if(a>c)
-        {
-            printf("%d",a);
+        printf("%d",a);
         }
-        else
+        else 
         {
-            printf("%d",c);
+            printf("%d",c); 
+            
         }
-    }
-    else
-    {
-        if(b>c)
+        }
+        else { if(b>c)
         {
             printf("%d",b);
-        }
-        else
-        {
-            printf("%d",c);
-        }
-    }
-    
-    
+            }
+            else
+            {
+                printf("%d",c); }
+                }
 }
