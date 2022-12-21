2202291e47e45c85b5fb319c1de96b2b8c300556
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2022-12-21 22:40:55 +0530

    #include<stdio.h> int main () {     int a,b;     scanf("%d",&a);     if(b=a%2,b==0)     {         printf("Even");         }         else         {             printf("Odd");                     }
    }

---
+++ Even_or_Odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    scanf("%d",&a);
+    if(b=a%2,b==0)
+    {
+        printf("Even");
+        }
+        else 
+        {
+            printf("Odd"); 
+                    }
+    
+}
