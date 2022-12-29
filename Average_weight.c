40c13d15e561644e1c03ddc42768bfcc5e5e5e40
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2022-12-29 21:49:15 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d%d",&a,&b,&c);     d=3*a-(c+b);     printf("%d",d);
    }

--- Average_weight.c
+++ Average_weight.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
-{ int a,b,c,d;
-scanf("%d%d%d",&a,&b,&c);
-d=3*a-(c+b);
-printf("%d",d);
+{
+    int a,b,c,d;
+    scanf("%d%d%d",&a,&b,&c);
+    d=3*a-(c+b);
+    printf("%d",d);
     
 }
