bc23e505f1cdea0855fd97385c11cca9843960f6
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-01-02 21:05:13 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",j);             }             printf(" ");             }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,14 +2,15 @@
 int main()
 {
     int i,j,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    scanf("%d",&n); 
+    for(i=n;i>=1;i--)
     {
         for(j=1;j<=i;j++)
         {
             printf("%d",j);
-        }
-        printf("
+            }
+            printf("
 ");
-    }
+            }
+    
 }
