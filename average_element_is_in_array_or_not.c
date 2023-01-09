6ba1e4a3a024d0c2b698011eeae2bbeec1132ba7
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-01-09 21:56:09 +0530

    #include<stdio.h> int main() {     int n,i,x,arr[100],c=0,sum=0,res;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum+=arr[i];     }     res=sum/n;     for(i=0;i<n;i++)     {     		if(arr[i]==res)     		{     			c=1;     			break; 			} 			else 			{ 				c=0; 			}     }     if(c==1)     {     	printf("True"); 	} 	if(c==0) 	{ 		printf("False"); 	} }

---
+++ average_element_is_in_array_or_not.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x,arr[100],c=0,sum=0,res;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    res=sum/n;
+    for(i=0;i<n;i++)
+    {
+    		if(arr[i]==res)
+    		{
+    			c=1;
+    			break;
+			}
+			else
+			{
+				c=0;
+			}
+    }
+    if(c==1)
+    {
+    	printf("True");
+	}
+	if(c==0)
+	{
+		printf("False");
+	}
+}
