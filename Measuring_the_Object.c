25a0e27b85acdec508f2c37ed958c4b7f9ff26ff
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-01-27 20:57:08 +0530

    #include<stdio.h> int main() {     int n,x,y,z;     scanf("%d%d%d%d",&n,&x,&y,&z);     if(n==x||n==y||n==z||n==x+y||n==x+z||n==y+z||n==x+y+z)     {         printf("YES");         }         else         { printf("NO"); }
    }

--- Measuring_the_Object.c
+++ Measuring_the_Object.c
@@ -3,12 +3,11 @@ int main()
 {
     int n,x,y,z;
     scanf("%d%d%d%d",&n,&x,&y,&z);
-    if(n==x||n==y||n==z||n==x+y||n==x+z||n==y+z||n==x+y+z)
-    {
+    if(n==x||n==y||n==z||n==x+y||n==x+z||n==y+z||n==x+y+z) 
+    { 
         printf("YES");
-    }
-    else
-    {
-        printf("NO");
-    }
+        }
+        else 
+        { printf("NO"); }
+    
 }
