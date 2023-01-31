290bdd62359e1a5a4143d0c7a4161814ed5d7f22
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-01-31 21:20:22 +0530

    #include<stdio.h> int main() {     int n,m,k;     scanf("%d%d%d",&n,&m,&k);     if(m*k>=n) printf("YES");     else printf("NO"); }

--- Wanda_and_Bread.c
+++ Wanda_and_Bread.c
@@ -1,15 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,k; 
+    int n,m,k;
     scanf("%d%d%d",&n,&m,&k);
-    if(m*k>=n)
-    {
-        printf("YES");
-        }
-        else 
-        {
-            printf("NO"); 
-            
-
-        }}
+    if(m*k>=n) printf("YES");
+    else printf("NO");
+}
