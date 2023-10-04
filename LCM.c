1bf2139c9bc0dfb6b79d4180c2a6dab81bb1f728
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-10-04 14:20:28 +0530

    #include<stdio.h> int main(){     int a,b,gcd;     scanf("%d%d",&a,&b);     for(int i=1;i<=a && i<=b;i++){         if(a%i==0 && b%i==0){             gcd=i;         }     }     printf("%d",(a*b)/gcd); }

--- LCM.c
+++ LCM.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main(){
-    int a,b;
+    int a,b,gcd;
     scanf("%d%d",&a,&b);
-    int gcd;
-    for(int i=1;i<=a && i<=b ;i++){
-        if (a%i==0 && b%i==0){
+    for(int i=1;i<=a && i<=b;i++){
+        if(a%i==0 && b%i==0){
             gcd=i;
         }
     }
-        printf("%d",(a*b)/gcd);
+    printf("%d",(a*b)/gcd);
 }
