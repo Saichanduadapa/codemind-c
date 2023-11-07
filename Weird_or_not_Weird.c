3666201ec37baf17f21202bb0406e7fcc4661d03
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-11-07 15:31:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2!=0)     {         printf("weird");     }     else if (n%2==0 && 2<=n && n<=5)     {         printf("not weird");     }     else if(n%2==0 && 6<=n && n<=20)     {         printf("weird");     }     else if(n%2==0 && n>20)     {         printf("not weird");     }
    }

--- Weird_or_not_Weird.c
+++ Weird_or_not_Weird.c
@@ -6,18 +6,18 @@ int main()
     if(n%2!=0) 
     {
         printf("weird");
-        }
-        else if (n%2==0 && 2<n>5) 
-        {
-            printf("not weird"); 
-                } 
-                else if(n%2==0 && 6<n>20)
-                {
-                    printf("weird");
-                    }
-                    else if(n%2==0 && n<20) 
-                    {
-                        printf("not weird");
-                        }
+    }
+    else if (n%2==0 && 2<=n && n<=5) 
+    {
+        printf("not weird"); 
+    } 
+    else if(n%2==0 && 6<=n && n<=20)
+    {
+        printf("weird");
+    }
+    else if(n%2==0 && n>20) 
+    {
+        printf("not weird");
+    }
     
 }
