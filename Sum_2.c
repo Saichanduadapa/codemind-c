411df8d5d8fa69cf7d0f749600880b5dfffe51b8
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-11-08 21:26:32 +0530

    #include<stdio.h> int main(){     int a,b,c,d,s=0;     scanf("%d%d%d%d",&a,&b,&c,&d);     for(int i=a;i<=b;i++){         if(i%c==0 && i%d!=0){             //printf("%d ",i);             s+=i;         }     }     printf("%d",s); }

---
+++ Sum_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,s=0;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    for(int i=a;i<=b;i++){
+        if(i%c==0 && i%d!=0){
+            //printf("%d ",i);
+            s+=i;
+        }
+    }
+    printf("%d",s);
+}
